20c8560cd2064bb585418ab8222b94bb40557813
Tayler Edelen <TaylerLEdelen@gmail.com>
2023-03-08 22:51:03 -0600

    Parts 1-2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://outline-prod.imgix.net/20191127-FUrZgJqlpdctKmNBTT4I?auto=format&q=60&w=1280&s=8a4c10f929fafc391c1afd9a191b2fa7';
   image3 = '';
 
   constructor() { }
